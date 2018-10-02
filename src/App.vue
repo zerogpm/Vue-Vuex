48bc5448e0d720c3cc90136d8bd2464b21406b80
zerogpm <zerogpm@gmail.com>
2018-10-02 10:03:42 -0700

    project set up

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Vuex</h1>
+        <app-result :counter="counter"></app-result>
+        <hr>
+        <app-counter @updated="counter += $event"></app-counter>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Counter from './components/Counter.vue';
+  import Result from './components/Result.vue';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    data() {
+      return {
+        counter: 0
+      }
+    },
+    components: {
+      appCounter: Counter,
+      appResult: Result,
+    }
   }
-}
 </script>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
